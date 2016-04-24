a8b049e5cf074a5d2b1c59eee16d726949b5f2be
Piotr Lukasik <piterpti@gmail.com>
2016-04-24 12:51:27 +0200

    Added adapter calss

---
+++ app/src/main/java/com/example/stacjonarny/graulamki/Classes/AchievementAdapter.java
@@ -0,0 +1,36 @@
+package com.example.stacjonarny.graulamki.Classes;
+
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.*;
+
+import com.example.stacjonarny.graulamki.R;
+
+
+public class AchievementAdapter extends ArrayAdapter {
+
+    public AchievementAdapter(Context context, ArrayList<Achievement> list) {
+        super(context, 0, list);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Achievement achievement = (Achievement) getItem(position);
+        if(convertView == null)
+        {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_achievement_adapter, parent, false);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.simpleAchievement);
+        String temporaryText = achievement.isLocked() ? " locked" : " unlocked";
+        String diffLevel = achievement.getDifficultLevel() + "";
+        textView.setText(achievement.getName() + temporaryText + " ,diffLevel = " + diffLevel);
+        return convertView;
+    }
+
+}

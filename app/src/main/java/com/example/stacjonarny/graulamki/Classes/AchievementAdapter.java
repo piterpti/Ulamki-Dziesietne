9652a4186b282449a5ce4d38136a79b6de74c524
Piotr Lukasik <piterpti@gmail.com>
2016-04-24 14:19:24 +0200

    Added new icon

--- app/src/main/java/com/example/stacjonarny/graulamki/Classes/AchievementAdapter.java
+++ app/src/main/java/com/example/stacjonarny/graulamki/Classes/AchievementAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.*;
 
@@ -15,6 +16,8 @@ import com.example.stacjonarny.graulamki.R;
 
 
 public class AchievementAdapter extends ArrayAdapter {
+
+    private ImageView image;
 
     public AchievementAdapter(Context context, ArrayList<Achievement> list) {
         super(context, 0, list);
@@ -29,12 +32,30 @@ public class AchievementAdapter extends ArrayAdapter {
         }
         TextView textView = (TextView) convertView.findViewById(R.id.simpleAchievement);
         TextView diffLevelView = (TextView) convertView.findViewById(R.id.achievementDifficultLevel);
+        image = (ImageView) convertView.findViewById(R.id.achievementListImage);
+        ImageChange(achievement.isLocked(), achievement.getDifficultLevel());
         String temporaryText = achievement.isLocked() ? " locked" : " unlocked";
-        String diffLevel = achievement.getDifficultLevel() + "";
         String achievementDiffLevel = MainActivity.difficultLevels[achievement.getDifficultLevel()-1].getLevel();
         textView.setText(achievement.getName() + temporaryText);
         diffLevelView.setText(achievementDiffLevel);
         return convertView;
+    }
+
+    private void ImageChange(boolean locked, int difficultLevel) {
+        if(!locked) {
+            if(difficultLevel <= 2) {
+                image.setImageResource(R.drawable.achievement_bronze);
+            }
+            else if(difficultLevel <=4) {
+                image.setImageResource(R.drawable.achievement_silver);
+            }
+            else {
+                image.setImageResource(R.drawable.achievement_gold);
+            }
+        }
+        else {
+            image.setImageResource(R.drawable.achievement_locked);
+        }
     }
 
 }
